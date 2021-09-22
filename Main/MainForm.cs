fb0bc876e2af2677e6412b13ec79fb2961a40c6a
Bean <bean@yuanfen.net>
2021-09-22 11:18:16 +0800

    WIP commit

--- Main/MainForm.cs
+++ Main/MainForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,6 +27,12 @@ namespace SczoneTavernDataCollector.Main
         private void MainForm_Load(object sender, EventArgs e)
         {
             Log("程序启动");
+            FindTavernBankFiles();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Log("程序退出");
         }
 
         private void Log(string text)
@@ -33,9 +40,27 @@ namespace SczoneTavernDataCollector.Main
             LogTextBox.Text += $"[{DateTime.Now.ToString("F")}] {text}\r\n";
         }
 
-        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        private void FindTavernBankFiles()
         {
-            Log("程序退出");
+            var starcraft2Path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "StarCraft II");
+            Log("星际2档案目录: " + starcraft2Path);
+            var accountsDirectory = new DirectoryInfo(Path.Combine(starcraft2Path, "Accounts"));
+            if (accountsDirectory.Exists)
+            {
+                foreach (var accountDirectory in accountsDirectory.GetDirectories())
+                {
+                    var accountNo = accountDirectory.Name;
+                    foreach (var directory in accountDirectory.GetDirectories())
+                    {
+                        Log(directory.Name);
+                    }
+                }
+            }
+            else
+            {
+                Log("Accounts 目录不存在：" + accountsDirectory.FullName);
+            }
         }
+
     }
 }
